840ae26246ae2b43d27c53fa477e03954831211e
Loki <github@kloki.com>
2025-08-01 21:27:51 +0200

    squared board

--- project/website/pages.py
+++ project/website/pages.py
@@ -5,26 +5,8 @@ from flask import render_template, send_from_directory, current_app, redirect, u
 from . import pages_blueprint
 
 
-columns = 12
-rows = 16
-
-# columns = 2
-# rows = 1
-
-# board = [
-# [{'row': 0, 'col': 0, 'tile': 'grass'}, {'row': 0, 'col': 1, 'tile': 'grass'}, {'row': 0, 'col': 2, 'tile': 'grass'}, {'row': 0, 'col': 3, 'tile': 'grass'}, {'row': 0, 'col': 4, 'tile': 'grass'}, {'row': 0, 'col': 5, 'tile': 'grass'}, {'row': 0, 'col': 6, 'tile': 'grass'}, {'row': 0, 'col': 7, 'tile': 'grass'}, {'row': 0, 'col': 8, 'tile': 'grass'}, {'row': 0, 'col': 9, 'tile': 'grass'}, {'row': 0, 'col': 10, 'tile': 'grass'}, {'row': 0, 'col': 11, 'tile': 'grass'}],
-#     [{'row': 1, 'col': 0, 'tile': 'grass'}, {'row': 1, 'col': 1, 'tile': 'grass'}, {'row': 1, 'col': 2, 'tile': 'grass'}, {'row': 1, 'col': 3, 'tile': 'grass'}, {'row': 1, 'col': 4, 'tile': 'grass'}, {'row': 1, 'col': 5, 'tile': 'grass'}, {'row': 1, 'col': 6, 'tile': 'grass'}, {'row': 1, 'col': 7, 'tile': 'grass'}, {'row': 1, 'col': 8, 'tile': 'grass'}, {'row': 1, 'col': 9, 'tile': 'grass'}, {'row': 1, 'col': 10, 'tile': 'grass'}, {'row': 1, 'col': 11, 'tile': 'grass'}],
-# [{'row': 2, 'col': 0, 'tile': 'grass'}, {'row': 2, 'col': 1, 'tile': 'grass'}, {'row': 2, 'col': 2, 'tile': 'grass'}, {'row': 2, 'col': 3, 'tile': 'grass'}, {'row': 2, 'col': 4, 'tile': 'grass'}, {'row': 2, 'col': 5, 'tile': 'grass'}, {'row': 2, 'col': 6, 'tile': 'grass'}, {'row': 2, 'col': 7, 'tile': 'grass'}, {'row': 2, 'col': 8, 'tile': 'grass'}, {'row': 2, 'col': 9, 'tile': 'grass'}, {'row': 2, 'col': 10, 'tile': 'grass'}, {'row': 2, 'col': 11, 'tile': 'grass'}],
-#     [{'row': 3, 'col': 0, 'tile': 'grass'}, {'row': 3, 'col': 1, 'tile': 'grass'}, {'row': 3, 'col': 2, 'tile': 'grass'}, {'row': 3, 'col': 3, 'tile': 'grass'}, {'row': 3, 'col': 4, 'tile': 'grass'}, {'row': 3, 'col': 5, 'tile': 'grass'}, {'row': 3, 'col': 6, 'tile': 'grass'}, {'row': 3, 'col': 7, 'tile': 'grass'}, {'row': 3, 'col': 8, 'tile': 'sand'}, {'row': 3, 'col': 9, 'tile': 'sand'}, {'row': 3, 'col': 10, 'tile': 'sand'}, {'row': 3, 'col': 11, 'tile': 'grass'}],
-# [{'row': 4, 'col': 0, 'tile': 'grass'}, {'row': 4, 'col': 1, 'tile': 'grass'}, {'row': 4, 'col': 2, 'tile': 'grass'}, {'row': 4, 'col': 3, 'tile': 'grass'}, {'row': 4, 'col': 4, 'tile': 'grass'}, {'row': 4, 'col': 5, 'tile': 'grass'}, {'row': 4, 'col': 6, 'tile': 'grass'}, {'row': 4, 'col': 7, 'tile': 'grass'}, {'row': 4, 'col': 8, 'tile': 'grass'}, {'row': 4, 'col': 9, 'tile': 'sand'}, {'row': 4, 'col': 10, 'tile': 'sand'}, {'row': 4, 'col': 11, 'tile': 'grass'}],
-#     [{'row': 5, 'col': 0, 'tile': 'grass'}, {'row': 5, 'col': 1, 'tile': 'grass'}, {'row': 5, 'col': 2, 'tile': 'grass'}, {'row': 5, 'col': 3, 'tile': 'grass'}, {'row': 5, 'col': 4, 'tile': 'grass'}, {'row': 5, 'col': 5, 'tile': 'grass'}, {'row': 5, 'col': 6, 'tile': 'grass'}, {'row': 5, 'col': 7, 'tile': 'grass'}, {'row': 5, 'col': 8, 'tile': 'grass'}, {'row': 5, 'col': 9, 'tile': 'sand'}, {'row': 5, 'col': 10, 'tile': 'grass'}, {'row': 5, 'col': 11, 'tile': 'grass'}],
-# [{'row': 6, 'col': 0, 'tile': 'grass'}, {'row': 6, 'col': 1, 'tile': 'grass'}, {'row': 6, 'col': 2, 'tile': 'grass'}, {'row': 6, 'col': 3, 'tile': 'grass'}, {'row': 6, 'col': 4, 'tile': 'grass'}, {'row': 6, 'col': 5, 'tile': 'grass'}, {'row': 6, 'col': 6, 'tile': 'grass'}, {'row': 6, 'col': 7, 'tile': 'grass'}, {'row': 6, 'col': 8, 'tile': 'grass'}, {'row': 6, 'col': 9, 'tile': 'grass'}, {'row': 6, 'col': 10, 'tile': 'grass'}, {'row': 6, 'col': 11, 'tile': 'grass'}],
-#     [{'row': 7, 'col': 0, 'tile': 'grass'}, {'row': 7, 'col': 1, 'tile': 'grass'}, {'row': 7, 'col': 2, 'tile': 'grass'}, {'row': 7, 'col': 3, 'tile': 'grass'}, {'row': 7, 'col': 4, 'tile': 'grass'}, {'row': 7, 'col': 5, 'tile': 'grass'}, {'row': 7, 'col': 6, 'tile': 'grass'}, {'row': 7, 'col': 7, 'tile': 'grass'}, {'row': 7, 'col': 8, 'tile': 'grass'}, {'row': 7, 'col': 9, 'tile': 'grass'}, {'row': 7, 'col': 10, 'tile': 'grass'}, {'row': 7, 'col': 11, 'tile': 'grass'}],
-# [{'row': 8, 'col': 0, 'tile': 'grass'}, {'row': 8, 'col': 1, 'tile': 'grass'}, {'row': 8, 'col': 2, 'tile': 'grass'}, {'row': 8, 'col': 3, 'tile': 'grass'}, {'row': 8, 'col': 4, 'tile': 'grass'}, {'row': 8, 'col': 5, 'tile': 'grass'}, {'row': 8, 'col': 6, 'tile': 'grass'}, {'row': 8, 'col': 7, 'tile': 'grass'}, {'row': 8, 'col': 8, 'tile': 'grass'}, {'row': 8, 'col': 9, 'tile': 'grass'}, {'row': 8, 'col': 10, 'tile': 'grass'}, {'row': 8, 'col': 11, 'tile': 'grass'}],
-#     [{'row': 9, 'col': 0, 'tile': 'grass'}, {'row': 9, 'col': 1, 'tile': 'grass'}, {'row': 9, 'col': 2, 'tile': 'grass'}, {'row': 9, 'col': 3, 'tile': 'grass'}, {'row': 9, 'col': 4, 'tile': 'grass'}, {'row': 9, 'col': 5, 'tile': 'grass'}, {'row': 9, 'col': 6, 'tile': 'grass'}, {'row': 9, 'col': 7, 'tile': 'grass'}, {'row': 9, 'col': 8, 'tile': 'grass'}, {'row': 9, 'col': 9, 'tile': 'grass'}, {'row': 9, 'col': 10, 'tile': 'grass'}, {'row': 9, 'col': 11, 'tile': 'grass'}],
-# [{'row': 10, 'col': 0, 'tile': 'grass'}, {'row': 10, 'col': 1, 'tile': 'grass'}, {'row': 10, 'col': 2, 'tile': 'grass'}, {'row': 10, 'col': 3, 'tile': 'grass'}, {'row': 10, 'col': 4, 'tile': 'grass'}, {'row': 10, 'col': 5, 'tile': 'grass'}, {'row': 10, 'col': 6, 'tile': 'grass'}, {'row': 10, 'col': 7, 'tile': 'grass'}, {'row': 10, 'col': 8, 'tile': 'grass'}, {'row': 10, 'col': 9, 'tile': 'grass'}, {'row': 10, 'col': 10, 'tile': 'grass'}, {'row': 10, 'col': 11, 'tile': 'grass'}],
-#     [{'row': 11, 'col': 0, 'tile': 'water'}, {'row': 11, 'col': 1, 'tile': 'water'}, {'row': 11, 'col': 2, 'tile': 'water'}, {'row': 11, 'col': 3, 'tile': 'water'}, {'row': 11, 'col': 4, 'tile': 'water'}, {'row': 11, 'col': 5, 'tile': 'water'}, {'row': 11, 'col': 6, 'tile': 'water'}, {'row': 11, 'col': 7, 'tile': 'water'}, {'row': 11, 'col': 8, 'tile': 'water'}, {'row': 11, 'col': 9, 'tile': 'water'}, {'row': 11, 'col': 10, 'tile': 'water'}, {'row': 11, 'col': 11, 'tile': 'water'}]
-# ]
+columns = 20
+rows = 32
 
 
 #######################################################
@@ -56,7 +38,7 @@ def index(msg=''):
 
 @pages_blueprint.route('/terraform/', methods=['GET'])
 def terraform():
-    if int(request.args.get('row')) > -1 or int(request.args.get('col')) > -1 or request.args.get('material') in ('grass, sand, water, mountain'):
+    if int(request.args.get('row')) > -1 or int(request.args.get('col')) > -1 or request.args.get('material') in ('grass, forest, sand, water, mountain'):
         file_path = 'board.json'
         with open(file_path, 'r') as file:
             data = json.load(file)
